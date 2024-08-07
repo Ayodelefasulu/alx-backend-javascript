5ace50ee609583d3b02e709fa096fffdbe7babce
root <root@DESKTOP-HO99MKF.>
2024-08-07 10:32:47 +0100

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,5 @@
-//var success = true;
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation, such as fetching data from an API
-    //success = true; // Change this to false to simulate a rejection
 
     if (success) {
       resolve({"status": "200", "body": "Success"});
