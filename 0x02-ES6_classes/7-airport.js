ad1affc31166fc3db3d2ec891dfa4005f5280d0d
root <root@DESKTOP-HO99MKF.>
2024-08-09 05:23:00 +0100

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+// 7-airport.js
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Override the toString method
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
+
