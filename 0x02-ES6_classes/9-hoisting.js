599584ae6b5e411f0f8ec162daf069c6148dc89a
root <root@DESKTOP-HO99MKF.>
2024-08-09 05:45:36 +0100

    task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,6 +1,6 @@
 // 9-hoisting.js
 
-export class HolbertonClass {
+export default class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -15,7 +15,7 @@ export class HolbertonClass {
   }
 }
 
-export class StudentHolberton {
+export default class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -46,6 +46,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export list of students
-export const listOfStudents = [student1, student2, student3, student4, student5];
+// export default list of students
+export default const listOfStudents = [student1, student2, student3, student4, student5];
 
